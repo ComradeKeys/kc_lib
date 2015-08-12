5edab2f72ba9054fc151c2f6c02cd232e0ae9834
dwc3993 <chr11031@byui.edu>
2015-08-12 00:25:09 -0600

    Implemented connection number recognition for server

--- skynet/kc_skynet.h
+++ skynet/kc_skynet.h
@@ -19,7 +19,7 @@ using namespace std;
 
 
 template <class from_Server, class from_Client>
-class kc_Client {
+  class kc_Client {
  private:
   // Shared memory stuff
   from_Server * shm_fromServer;
@@ -157,7 +157,7 @@ class kc_Client {
   // How we write to the server
   void writeData(from_Client input) {
     if(*writePending);
-      //!;cout << "\t\tWRITE PENDING IN WRITE-DATA FUNCTION\n";
+    //!;cout << "\t\tWRITE PENDING IN WRITE-DATA FUNCTION\n";
 
     //!cout << "ON THE USER LEVEL P: " << writePending << endl;
     //!cout << "\tin this 'writeData' we are inserting: " << input << endl;
@@ -190,10 +190,10 @@ class kc_Client {
             while(*writePending) { 
 	      ;
 	      /*
-	      if(!*writePending)
+		if(!*writePending)
 		cout << "\t\tWRITE NOTE PENDING\n";
-	      else
-	      cout << "\t\tWRITE PENDING\n"; */
+		else
+		cout << "\t\tWRITE PENDING\n"; */
 	    }
 	    //!cout << "\tWriting to the server\n";
             rv = write(sockfd, (void *)shm_fromClient, sizeof(from_Client));
@@ -219,9 +219,14 @@ class kc_Client {
   void turnOff() {*runClient = false;}
 };
 
+//Linked-list node for connection tracking (the number of each connection)
+struct connection{
+  int num;
+  connection * next;
+};
 
 template <class in_Server, class from_Server, class from_Client>
-class kc_Server {
+  class kc_Server {
  private:
   // Shared memory stuff
   in_Server * shm_inServer;
@@ -230,16 +235,13 @@ class kc_Server {
   bool * childFlag;
   int  * numConnections;
   int * maxConnections;
+  connection *headCon;
   from_Server (*function)(in_Server*, from_Client*, int); // The function that the client-server operates on. Each thread processes data here.
    
   // Strictly networking stuff 
   int sockfd, newsockfd, clienlen;
   int rv; // Error checking
   struct sockaddr_in serv_addr, cli_addr;
-
-  int secureId() {
-    return 5;//BLARG -> FIX THIS YESTERDAY
-  }
 
   void initSharedMemory(int maxConn)
   {
@@ -257,7 +259,6 @@ class kc_Server {
       perror("shmat");
       return;
     }
-    //shm_inServer = new in_Server;      
 
     // Build our canRead bools info segment      
     key = 9897;
@@ -313,7 +314,6 @@ class kc_Server {
       perror("shmat");
       return;
     }
-    //numConnections = new int;
     *numConnections = 0;
 
     key = 2797;
@@ -329,6 +329,19 @@ class kc_Server {
     }
     //maxConnections = new int;
     *maxConnections = maxConn;
+
+    key = 3613;
+    if((shmid = shmget(key, sizeof(connection), IPC_CREAT | 0666)) < 0) {
+      perror("shmget");
+      return;
+    }
+      
+    //Attach the max connections number to our server
+    if((int)(headCon = (connection *)shmat(shmid, NULL, 0)) == -1) {
+      perror("shmat");
+      return;
+    }
+    headCon->next = NULL;
   }
    
  public:
@@ -376,6 +389,51 @@ class kc_Server {
     return *numConnections;
   }
 
+  // Make a connection available on our linked list
+  void removeConnection(int connectionNum) {
+    connection * cursor = headCon;
+    if(connectionNum > *numConnections)
+      return;
+    for(int i = 0; i < connectionNum; i++) {
+      cursor = cursor->next;
+    }
+    if(!cursor->num) {
+      --*numConnections;
+      cursor->num = -1;    
+    }
+  }
+
+  // Allocate a connection number for the newly connected client
+  int getConnectionNum() {
+    if(*numConnections > *maxConnections)
+      return -1;
+    int con = 0;
+    connection * cursor = headCon;
+    ++*numConnections; // We're getting another client, so add one
+    if(!cursor) { // If this is the first connection
+      headCon = new connection;
+      cursor = headCon;
+      cursor->next = NULL;
+      return cursor->num = con;
+    }
+    else {
+      // If there is an available node in our list, use it
+      while(cursor->next) {
+	if(cursor->num == -1)
+	  return cursor->num = con;
+	cursor = cursor->next;
+	++con;
+      }
+      if(cursor->num == -1) return cursor->num = con;
+
+      // If we need to make space for a new node
+      cursor->next = new connection;
+      cursor = cursor->next;
+      cursor->next = NULL;
+      return cursor->num = *numConnections;
+    }
+  }
+
   int getMaxPlayers() {
     return *maxConnections;
   }
@@ -392,8 +450,7 @@ class kc_Server {
     *serverRunning = false;
   }
 
-  void start()
-  {
+  void start() {
     // Dynamic size made conveniently available in this variable
     clienlen = sizeof(cli_addr);
 
@@ -402,22 +459,22 @@ class kc_Server {
     //pid = fork(); 
      
     /*(
-    if(pid) // Get the parent process out of here.
+      if(pid) // Get the parent process out of here.
       return;
-    cout << "Spawning server listener process\n";
-    while(*serverRunning) { 
-    *childFlag = true; */
+      cout << "Spawning server listener process\n";
+      while(*serverRunning) { 
+      *childFlag = true; */
     while(pid) { // Add conditional for the server being running or NOT running
       cout << "\tMASTER_PROCESS: Listening for clients...\n";
       newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (socklen_t *)&clienlen);
       pid = fork();
       /*
-      if(*childFlag) { // If we can't use PID values then let's use shared memory!
+	if(*childFlag) { // If we can't use PID values then let's use shared memory!
 	cout << "\tTRUE FOR CHILD FLAG\n";
 	*childFlag = false;
 	break;
-      }
-      else {
+	}
+	else {
 	cout << "\tFALSE FOR CHILD FLAG\n";
 	} */
     }
@@ -428,10 +485,9 @@ class kc_Server {
       cout << "Error " << "Error on accept\n";
       return;
     }
-    else 
-      {
+    else {
 	cout << "\tPROCESS: Connection spanned...\n";
-	int idNumber = secureId();
+	int idNumber = getConnectionNum();
 	from_Client input;
 	from_Server response; 	
 	while(*serverRunning) 
@@ -460,7 +516,7 @@ class kc_Server {
 	      cout << "Error!\n" << rv << " (Error writing to socket)\n";
 	    }
 	    //else
-	      //!cout << "\t\tWrote back " << response << " to the client\n";
+	    //!cout << "\t\tWrote back " << response << " to the client\n";
 	  }
       }
     // Close this server connection here. 
